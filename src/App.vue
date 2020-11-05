80ecf6181d112228e6939a44a8bfaca0f04c6eb1
Jac <lu7766lu7766@gmail.com>
2020-11-05 23:36:17 +0800

    add composition api; change component name

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <iBikeMarathon />
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import iBikeMarathon from './components/iBikeMarathon.vue';
 
-export default Vue.extend({
+export default {
   name: 'App',
   components: {
-    HelloWorld
+    iBikeMarathon
   }
-});
+}
 </script>
-
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import VueCompositionAPI from '@vue/composition-api'
 
+Vue.use(VueCompositionAPI)
 Vue.config.productionTip = false
 
 new Vue({
