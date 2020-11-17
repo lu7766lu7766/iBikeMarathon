8cfcb0777ed8aa84c13bf8bf6a5ae6e3d9669f78
Jac <lu7766lu7766@gmail.com>
2020-11-17 10:13:19 +0800

    修正響應式問題

--- src/components/iBikeMarathon.vue
+++ src/components/iBikeMarathon.vue
@@ -19,12 +19,14 @@
               </b-input-group-append>
             </b-input-group>
           </b-form-group>
-          <b-form-group :label="stations.length ? '下一站' : '第一站(步行)'">
+          <b-form-group
+            :label="state.stations.length ? '下一站' : '第一站(步行)'"
+          >
             <b-input-group>
               <select class="form-control" v-model="selectedStationID">
                 <option value="">請選擇</option>
                 <option
-                  v-for="position in positions"
+                  v-for="position in state.positions"
                   :key="position.ID"
                   :value="position.ID"
                 >
@@ -42,7 +44,7 @@
               hover
               foot-clone
               caption-top
-              :items="stations"
+              :items="state.stations"
               :fields="[
                 { key: 'index', label: '站數' },
                 { key: 'name', label: '站名' },
@@ -55,7 +57,7 @@
               </template>
 
               <template #cell(index)="data">
-                {{ data.index + 1 }}
+                {{ data.index + 1 }}{{ !data.index ? '(步行5km/hr)' : '' }}
               </template>
               <template #cell(distance)="data">
                 {{ data.item.distance + "km" }}
@@ -65,9 +67,15 @@
               </template>
 
               <template #foot(index)> 小計 </template>
-              <template #foot(name)> 共計{{ stations.length }}站 </template>
-              <template #foot(distance)> {{ countDistance() }}km </template>
-              <template #foot(costTime)> {{ countCostTime() }}hr </template>
+              <template #foot(name)>
+                共計{{ state.stations.length }}站
+              </template>
+              <template #foot(distance)>
+                {{ state.total.distance }}km
+              </template>
+              <template #foot(costTime)>
+                {{ state.total.costTime }}hr
+              </template>
             </b-table>
           </b-form-group>
         </b-overlay>
@@ -94,7 +102,7 @@ export default {
     const isLoading = ref(true)
     const rideSpeed = ref(10)
     const selectedStationID = ref("")
-    const stations = reactive([])
+    // const stations = reactive([])
     const options = reactive({
       positions: []
     })
@@ -113,36 +121,58 @@ export default {
       options.positions = res.data
     }
     const onSubmit = () => {
-      const distance = CountDistance(location.lat, location.lng, selectedStation.value.Y, selectedStation.value.X).toFixed(2)
+      const distance = CountDistance(location.lat, location.lng, state.selectedStation.Y, state.selectedStation.X).toFixed(2)
       // 第一站為步行時速
       const WALK_SPEED = 5
-      const realSpeed = stations.length ? rideSpeed.value : WALK_SPEED
+      const realSpeed = state.stations.length ? rideSpeed.value : WALK_SPEED
       const costTime = new Decimal(distance).div(realSpeed).toFixed(2)
-      stations.push({
-        'name': selectedStation.value.name,
+      state.stations.push({
+        'name': state.selectedStation.name,
         'distance': distance,
         'costTime': costTime,
       })
-      location.lat = selectedStation.value.Y
-      location.lng = selectedStation.value.X
+      location.lat = state.selectedStation.Y
+      location.lng = state.selectedStation.X
     }
 
-    const positions = computed(() => {
-      return _.orderBy(options.positions.map(x => {
-        const distance = CountDistance(location.lat, location.lng, x.Y, x.X)
+    // const positions = computed(() => {
+    //   return _.orderBy(options.positions.map(x => {
+    //     const distance = CountDistance(location.lat, location.lng, x.Y, x.X)
+    //     return {
+    //       ...x,
+    //       distance,
+    //       displayName: `${x.Position} ${distance.toFixed(2)}km`,
+    //       name: x.Position
+    //     }
+    //   }), 'distance')
+    // })
+    const state = reactive({
+      stations: [],
+      positions: computed(() => {
+        return _.orderBy(options.positions.map(x => {
+          const distance = CountDistance(location.lat, location.lng, x.Y, x.X)
+          return {
+            ...x,
+            distance,
+            displayName: `${x.Position} ${distance.toFixed(2)}km`,
+            name: x.Position
+          }
+        }), 'distance')
+      }),
+      selectedStation: computed(() => _.find(state.positions, x => x.ID === selectedStationID.value)),
+      total: computed(() => {
+        const IBIKE_BORROW_RETURN_COST_TIME = 0.5
         return {
-          ...x,
-          distance,
-          displayName: `${x.Position} ${distance.toFixed(2)}km`,
-          name: x.Position
+          distance: state.stations.reduce((res, x) => new Decimal(res).plus(x.distance).toFixed(2), 0),
+          costTime: state.stations.reduce((res, x, i) => new Decimal(res).plus(x.costTime).plus(i ? IBIKE_BORROW_RETURN_COST_TIME : 0).toFixed(2), 0)
         }
-      }), 'distance')
+      })
     })
-    const selectedStation = computed(() => _.find(positions.value, x => x.ID === selectedStationID.value))
-    const countDistance = () => stations.reduce((res, x) => new Decimal(res).plus(x.distance).toFixed(2), 0)
+    // const selectedStation = computed(() => _.find(positions.value, x => x.ID === selectedStationID.value))
+    // const countDistance = () => stations.reduce((res, x) => new Decimal(res).plus(x.distance).toFixed(2), 0)
     // iBike租還時間
-    const IBIKE_BORROW_RETURN_COST_TIME = 0.5
-    const countCostTime = () => stations.reduce((res, x, i) => new Decimal(res).plus(x.costTime).plus(i ? IBIKE_BORROW_RETURN_COST_TIME : 0).toFixed(2), 0)
+    // const IBIKE_BORROW_RETURN_COST_TIME = 0.5
+    // const countCostTime = () => stations.reduce((res, x, i) => new Decimal(res).plus(x.costTime).plus(i ? IBIKE_BORROW_RETURN_COST_TIME : 0).toFixed(2), 0)
     // computed not work is bug??
     // const total = computed(() => {
     //   const IBIKE_BORROW_RETURN_COST_TIME = 0.5
@@ -161,10 +191,11 @@ export default {
       selectedStationID,
       isLoading,
       rideSpeed,
-      positions,
-      stations,
-      countDistance,
-      countCostTime,
+      state,
+      // positions,
+      // stations,
+      // countDistance,
+      // countCostTime,
       onSubmit
     }
   }
